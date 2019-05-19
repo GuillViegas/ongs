a6dd310fadb786df763987ffd61d38d5ee1d2205
GuilViegas <guivfaria@gmail.com>
2019-05-19 16:25:48 -0300

    Core business

--- ongs/settings.py
+++ ongs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfigs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
